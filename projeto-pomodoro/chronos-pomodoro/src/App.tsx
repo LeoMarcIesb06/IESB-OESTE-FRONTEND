286df4217dd68c9b76d9819b747f9c2d963f4ee7
Marcelo <marcelo.v.soares@iesb.edu.br>
2026-05-24 20:15:44 -0300

    feat: organização e implementação da logo

--- projeto-pomodoro/chronos-pomodoro/src/App.tsx
+++ projeto-pomodoro/chronos-pomodoro/src/App.tsx
@@ -1,6 +1,7 @@
 // src/App.tsx
-import { Container } from './componentes/Container/Container';
-import { Heading } from './componentes/Heading/Heading';
+import { Container } from './componentes/Container';
+import { Heading } from './componentes/Heading';
+import { Logo } from './componentes/Logo';
 import './styles/themes.css';
 import './styles/global.css';
 
@@ -9,7 +10,7 @@ export function App() {
     <>
       {/* Seção 1: Logo */}
       <Container>
-        <Heading>Logo</Heading>
+        <Logo />
       </Container>
 
       {/* Seção 2: Menu */}
